bd03da68e1d123dbb611d6e010ebf3a0c2a55a82
leewoo <dlguddn3214@naver.com>
2020-01-02 16:53:40 +0900

    비구조화할당

---
+++ reactjsIntro/movie_app/src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+function Food({fav}){
+  return <h1>I like {fav}</h1>
+}
+
+function App() {
+  return (
+    <div>
+      <h1>Hello</h1>
+        <Food 
+            fav="kimchi" 
+        />
+        <Food 
+            fav="samgiopsal" 
+        />
+    </div>
+  );
+}
+
+export default App;
