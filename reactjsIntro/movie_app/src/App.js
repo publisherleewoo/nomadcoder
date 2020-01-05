0e9fbb5a8d8bc7e28e7584d72d678f015562c494
leewoo <dlguddn3214@naver.com>
2020-01-05 19:30:52 +0900

    no message

--- reactjsIntro/movie_app/src/App.js
+++ reactjsIntro/movie_app/src/App.js
@@ -1,7 +1,9 @@
 import React, { PureComponent } from 'react';
+import Movie from "./Movie"
+import axios from "axios";
 
 class App extends PureComponent {
-  constructor(props){
+  constructor(props) {
     super(props);
     console.log("constructor")
   }
@@ -10,21 +12,38 @@ class App extends PureComponent {
     movies: []
   }
 
-   
-  componentDidMount(){
-    console.log("component did mount")
-    setTimeout(() => {
-      this.setState({ isLoading: false });
-    }, 6000);
+  getMovides = async () => {
+    const { data: { data: { movies } } } = await axios.get("https://yts-proxy.now.sh/list_movies.json?sort_by=rating")
+    this.setState({ movies, isLoading: false })
   }
-  
-  componentDidUpdate(){
+
+  componentDidMount() {
+    console.log("component did mount")
+    this.getMovides();
+  }
+
+  componentDidUpdate() {
     console.log("component did update")
   }
   render() {
     console.log("render");
-    const { isLoading } = this.state;
-    return <div>{isLoading ? "Loading..." : "We are ready"}</div>;
+    const { isLoading, movies } = this.state;
+    return <div>
+      {isLoading ? "Loading..."
+        :
+        movies.map(movie => {
+          console.log(movie)
+          return <Movie
+            key={movie.id}
+            id={movie.id}
+            year={movie.year}
+            title={movie.title}
+            summary={movie.summary}
+            poster={movie.medium_cover_image}
+          />
+        })
+      }
+    </div>;
   }
 }
 export default App;
